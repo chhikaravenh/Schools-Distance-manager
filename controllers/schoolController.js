15ba108bf5954a2c1b05914504be400e8a069914
chhikaravenh <chhikara.venh@gmail.com>
2025-08-07 21:03:43 +0530

    Initial backend commit

---
+++ controllers/schoolController.js
@@ -0,0 +1,32 @@
+const db = require('../config/db');
+
+// Add School API
+exports.addSchool = (req, res) => {
+  const { name, address, latitude, longitude } = req.body;
+
+  if (!name || !address || !latitude || !longitude) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  const sql = 'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)';
+  db.query(sql, [name, address, latitude, longitude], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(201).json({ message: 'School added successfully' });
+  });
+};
+
+// List Schools API
+exports.listSchools = (req, res) => {
+  const { latitude, longitude } = req.query;
+
+  if (!latitude || !longitude) {
+    return res.status(400).json({ message: 'Latitude and Longitude are required' });
+  }
+
+  const sql = 'SELECT *, (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance FROM schools ORDER BY distance ASC';
+
+  db.query(sql, [latitude, longitude, latitude], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+};
